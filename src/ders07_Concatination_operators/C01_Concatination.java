a33757ef9da8c0d7097aae6a196d9bf40b1324c4
Ebubekir Tosun <tosunbekir91@gmail.com>
2023-08-20 21:24:37 +0200

    newnew

---
+++ src/ders07_Concatination_operators/C01_Concatination.java
@@ -0,0 +1,10 @@
+package ders07_Concatination_operators;
+
+public class C01_Concatination {
+    public static void main(String[] args) {
+        int a= 10 ;
+        int b = 20 ;
+
+        System.out.println("Verilen iki degerin toplami :"+(a+b));
+    }
+}
