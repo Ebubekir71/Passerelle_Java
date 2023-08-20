e10dbebab748b505b7243f69f8cd691b68636801
Ebubekir Tosun <tosunbekir91@gmail.com>
2023-08-20 21:03:40 +0200

    new

---
+++ src/ders06_Increment_Decrement/C02_Preincrement_Postincrement.java
@@ -0,0 +1,18 @@
+package ders06_Increment_Decrement;
+
+public class C02_Preincrement_Postincrement {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(+ ++a);
+
+        int b = a++;
+        System.out.println(+ b);
+
+        int c = b++ + a;
+
+        System.out.println(+ c);
+
+
+
+    }
+}
