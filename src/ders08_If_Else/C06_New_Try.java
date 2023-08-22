c4d72f88557e4275b092875d61d71c5f77a7fa2e
Ebubekir Tosun <tosunbekir91@gmail.com>
2023-08-22 19:11:52 +0200

    new

--- src/ders08_If_Else/C06_New_Try.java
+++ src/ders08_If_Else/C06_New_Try.java
@@ -1,4 +1,16 @@
 package ders08_If_Else;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class C06_New_Try {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen bir sayi girin");
+        int a = scan.nextInt();
+
+        a = a>0 ? a*2 : a+10;
+        System.out.println(a);
+    }
 }
